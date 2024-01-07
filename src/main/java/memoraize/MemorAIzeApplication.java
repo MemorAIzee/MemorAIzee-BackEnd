871f328a638b978a6e6162f4a6948a6ba717923c
junhaa <2171326@hansung.ac.kr>
2024-01-07 10:41:04 +0900

    :tada: Setup

---
+++ src/main/java/memoraize/MemorAIzeApplication.java
@@ -0,0 +1,13 @@
+package memoraize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorAIzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorAIzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/memoraize/MemorAIzeApplicationTests.java
@@ -0,0 +1,13 @@
+package memoraize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorAIzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
